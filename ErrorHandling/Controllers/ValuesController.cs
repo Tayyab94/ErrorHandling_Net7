7483a61d90bc9cb6a291ea2c510836a88f9fb101
Tayyab94 <tayyab.bhatti30@gmail.com>
2023-07-14 16:26:38 +0500

    Frist Commit

---
+++ ErrorHandling/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using ErrorHandling.Models;
+using LoggerService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorHandling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private List<Student> students = new List<Student>();
+        private ILoggerManager _logger;
+
+        public ValuesController(ILoggerManager logger)
+        {
+            _logger = logger;
+            students = new List<Student>()
+            {
+                new Student()
+            {
+                Id=1,
+                Name="First Name",
+            },
+            new Student()
+            {
+                Id= 2,
+                Name="Second Name"
+            },new Student() { Id=3, Name="Third Name"}
+            };
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+       
+                _logger.LogInfo("Fetching all the Students from the storage");
+
+                throw new Exception("Exception while fetching all the students from the storage.");
+                _logger.LogInfo($"Returning {students.Count} students.");
+                return Ok(students);
+
+        }
+
+
+    }
+}
---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,40 @@
+using ErrorHandling.Extensions;
+using LoggerService;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nglog.config"));
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ILoggerManager, LoggerManager>();
+
+var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+
+if (app.Environment.IsProduction())
+    app.UseHsts();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //app.ConfigureExceptionHandler(logger);
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
